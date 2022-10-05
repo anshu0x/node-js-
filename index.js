d7be52643499a4f7316a438e94e73f089af93d2a
anshu0x <anshusharma6327@gmail.com>
2022-10-05 14:20:58 +0530

    Template Engines (Pug, hbs, EJS) in Node JS 😍 | Add Dynamic Content in Express JS in Hindi

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const staticPath = path.join(__dirname,'/public')
+const staticPath = path.join(__dirname, '/public')
+const hbs = require('hbs');
+
 app.use(express.static(staticPath))
+//  we hav eto set view engine 
+app.set('view engine', 'hbs');
+//  template engine root
 
-
-app.get("/asd",(req,res)=>{
-  res.send("hea");
+app.get('/a', (req, res) => {
+    res.render('index',{
+        anshu: "anshu sharma "
+    })
+})
+//  it will not show because the connection was already closed  
+app.get("/", (req, res) => {
+    res.send("hell ofrom ansh")
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("app is running");
 })
