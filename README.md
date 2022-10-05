5b5a8623304724aebbbb0138e88d8f4d9930808f
anshu0x <anshusharma6327@gmail.com>
2022-10-05 14:51:20 +0530

    #8: Using Partials 😍 in Express JS in Hindi

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node-js-
+
+
+# nodemon index.js -e hbs,html,js   
+
+# for monitoring all type files
--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const staticPath = path.join(__dirname, '/public')
-const templatePath = path.join(__dirname,'templates')
-
+const templatePath = path.join(__dirname, 'templates/views')
+const partialsPath = path.join(__dirname, 'templates/partials')
+const hbs = require('hbs')
 app.use(express.static(staticPath))
 //  we hav eto set view engine 
 app.set('view engine', 'hbs');
-
-app.set('views',templatePath)
-
+app.set('views', templatePath)
+//  here we are using 
+hbs.registerPartials(partialsPath)
 //  template engine root
-
-app.get('/a', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index', {
-        anshu: "anshu sharma "
+        anshu: "anshu sharma ",
     })
 })
-//  it will not show because the connection was already closed  
-app.get("/", (req, res) => {
-    res.send("hell ofrom ansh")
+app.get('/about', (req, res) => {
+    res.render('about')
 })
+//  it will not show because the connection was already closed  
 
 app.listen(3000, () => {
     console.log("app is running");
